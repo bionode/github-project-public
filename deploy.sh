e6b5b6628550a0f3fb6e93296879102bac9319fb
IsmailM <ismail.moghul@gmail.com>
2017-04-19 02:51:00 +0100

    use jq's --slurp mode to convert the stream input into an array

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,7 @@ mkdir www;
 rm _data.json
 # run projects.js to pull the latest data from github Project
 node projects.js bionode | \
-  sed -e 's|$|,|' -e '$s|,$||' | \
-  (echo '[' && cat - && echo ']') | \
-  jq 'group_by(.column_name) |
+  jq -s 'group_by(.column_name) |
     map( { (.[0].column_name|tostring) : .  }) |
     add |
     {
